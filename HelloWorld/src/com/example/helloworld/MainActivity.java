86c4c76fc4d3079266cb9bdf24935b8f562cd700
sudhanshusri16 <ssrivastava@surveyanalytics.com>
2014-11-29 12:07:15 +0530

    First Commit

---
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.PorterDuff.Mode;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		  setContentView(R.layout.activity_main);
+	        VideoView v1 = (VideoView)findViewById(R.id.v1);
+	        v1.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.v));
+	        VideoView v2 = (VideoView)findViewById(R.id.v2);
+	        v2.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.vv));
+	        v1.start();
+	        v2.start();
+	        v2.setBackgroundColor(Color.parseColor("#00000000"));
+	        v2.setCameraDistance(45.0f);
+	        v2.setDrawingCacheEnabled(true);
+//	        v2.setBackgroundTintMode(Mode.CLEAR);
+	        v2.setZOrderOnTop(true);
+	}
+}
