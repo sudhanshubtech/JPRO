fcda1dc4b38f4beb0465aa6da7734cdb59782d73
sudhanshusri16 <ssrivastava@surveyanalytics.com>
2014-11-29 12:15:33 +0530

    deleted few files from raw folder

--- HelloWorld/src/com/example/helloworld/MainActivity.java
+++ HelloWorld/src/com/example/helloworld/MainActivity.java
@@ -14,15 +14,14 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		  setContentView(R.layout.activity_main);
 	        VideoView v1 = (VideoView)findViewById(R.id.v1);
-	        v1.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.v));
+//	        v1.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.v));
 	        VideoView v2 = (VideoView)findViewById(R.id.v2);
-	        v2.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.vv));
+//	        v2.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" +R.raw.vv));
 	        v1.start();
 	        v2.start();
 	        v2.setBackgroundColor(Color.parseColor("#00000000"));
 	        v2.setCameraDistance(45.0f);
 	        v2.setDrawingCacheEnabled(true);
-//	        v2.setBackgroundTintMode(Mode.CLEAR);
 	        v2.setZOrderOnTop(true);
 	}
 }
